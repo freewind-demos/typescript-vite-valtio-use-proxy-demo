f4bc8dc795b8564aab8b42976bb7806e92140cb2
Freewind <nowindlee@gmail.com>
2022-10-31 20:46:17 +0800

    typescript-vite-valtio-state-management-demo

--- README.md
+++ README.md
@@ -1,5 +1,16 @@
-TypeScript Vite React Demo
+TypeScript Vite React Valtio State Management Demo
 ===========================
+
+由于真正工作的是proxy，所以如果引用到了非proxy字段，就不会起作用。
+
+几个注意点：
+
+1. 对象中可以使用`as type`来明确类型
+2. action中修改字段时，要引用proxy之后的对象
+3. 使用store时，必须使用`store.action`风格，而不能先解构为`const {action} = store; action()`。因为这种情况下，proxy也会失效。
+
+最好按这里提示的方法来组织 actions: 
+https://github.com/pmndrs/valtio/wiki/How-to-organize-actions
 
 ```
 npm install
--- src/Hello.tsx
+++ src/Hello.tsx
@@ -1,10 +1,23 @@
-import React, { FC } from 'react';
+import React, {FC} from 'react';
 import './Hello.pcss';
+import {proxy, useSnapshot} from 'valtio';
 
-type Props = {};
+const store = proxy({
+    user: 'AAA' as string,
+    changeName: (value: string) => {
+      console.log("### new value: ", value)
+      // Use `store` here
+      store.user = value
+    },
+    hello: () => console.log(`Hello, ${store.user}`)
+  }
+);
 
-export const Hello: FC<Props> = ({}) => {
+export const Hello: FC = () => {
+  const {user} = useSnapshot(store);
+
   return <div className={'Hello'}>
-    <h1>Hello React</h1>
+    <h1>Hello {user}</h1>
+    <input type={'text'} value={user} onChange={(event) => store.changeName(event.target.value)}/>
   </div>;
 }
